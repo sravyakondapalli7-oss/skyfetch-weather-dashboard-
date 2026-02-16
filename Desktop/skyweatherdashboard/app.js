78897fce5d43902b24105c5bb52b7e998fc04a37
k sravya <sravya.kondapalli.s.144@kalvium.community>
2026-02-16 10:23:55 +0530

    Part 1: Basic API Integration

--- Desktop/skyweatherdashboard/app.js
+++ Desktop/skyweatherdashboard/app.js
@@ -1,4 +1,4 @@
-const API_KEY = "51ed2539fd3ad1e454aaa0dd312fd595";
+const API_KEY = "51ed2539fd3ad1e454aaa0dd312fd595git";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 function getWeather(city) {
